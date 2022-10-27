08435b6a316e108ba6cf99ab17e7b60401c6f7cf
Divya_sri_Boppudi <s556453@nwmissouri.edu>
2022-10-26 21:37:49 -0500

    updated computation

--- routes/computation.js
+++ routes/computation.js
@@ -8,10 +8,7 @@ router.get('/', function(req, res, next) {
   let f1 = Math.atan2(y, x);
   let f2 = Math.atanh(x);
   let f3 = Math.cbrt(x);
-  res.send(`<title>Computation</title><h1>Computing Math functions</h1><b style="color: red">atan2(y, x)</b> applied to <i style="color: blue">${x}</i>
-   and <i style="color: blue">${y}</i>  is  <i style="color: orange">${f1}</i><br><br>
-   <b style="color: red">atanh(x)</b> applied to <i style="color: blue">${x}</i>  is  <i style="color: orange">${f2}</i><br><br>
-   <b style="color: red">cbrt(x)</b> applied to <i style="color: blue">${x}</i>  is  <i style="color: orange">${f3}</i>`);
+  res.render('computation',{title: 'Computation', h1: 'Computing Math functions', x: x, y: y, f1:f1, f2:f2, f3:f3});
   });
 
 module.exports = router;
